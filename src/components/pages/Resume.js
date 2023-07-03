de736a6bf514a731d487f49377fa04f986a57fdc
csullins <camilesullins@gmail.com>
2023-07-02 19:09:31 -0500

    can't figure out deployment pathing

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import '../../styles/resume.css';
-import me from '../../assets/images/me.jpeg'
 export default function Resume() {
     return (
       <div className="resume">
